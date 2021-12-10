7270ef0923fa7f95cf391f2efaa74ce70cf26c65
Thomas <thomasbparr@gmail.com>
2021-12-10 09:45:39 -0600

    interim planning update

--- Day6.R
+++ Day6.R
@@ -2,7 +2,38 @@
 #Functional Programming
 #Iteration with functions, Vectorization, Best Practices
 
+
+#What is a function
+#why write a function
+#consistency - garbage in garbage out
+
+
+#Modifying simple function
+#naming a function and conventions
+
+#simple iteration
+#example using map or apply family should operate on one dataframe
+
+
+#curly brackets vs no
+
+#Coding best practices
+#avoiding loops within loops within... when you can
+
+
+
+
+
 ## JP Section
 
 
+
+
+
 ## Thomas Section
+
+
+
+
+
+
