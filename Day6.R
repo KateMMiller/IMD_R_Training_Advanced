7f7c6b5d236dbfa2f6cc86c3e6ee74080675647d
John Paul Schmit <jschmit@nps.gov>
2021-12-10 10:27:43 -0500

    Added Secitons for each of us

--- Day6.R
+++ Day6.R
@@ -1,3 +1,8 @@
 ###February 8 - Day 6 2022 R Training for IMD Scientists' Meeting
 #Functional Programming
 #Iteration with functions, Vectorization, Best Practices
+
+## JP Section
+
+
+## Thomas Section
